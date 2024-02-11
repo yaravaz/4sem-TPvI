08a7b8b53100ddf562e9cf1585787b1748d4d93e
Yaroslava Vovna <yaravazpoz@gmail.com>
2024-02-11 21:35:05 +0300

    add new class Car

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //TODO print string "Hello world"
         System.out.println("Hello world!");
+        Car car = new Car();
     }
 }
